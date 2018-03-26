e28e8bf0ef823379d1c5247d32356fe376db21ec
Gianni Genovesi <giannign1@gmail.com>
2018-03-26 18:54:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNCustomerlyReact } = NativeModules;
+
+export default RNCustomerlyReact;
---
+++ ios/RNCustomerlyReact.m
@@ -0,0 +1,13 @@
+
+#import "RNCustomerlyReact.h"
+
+@implementation RNCustomerlyReact
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
