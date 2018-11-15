1ba2a20937ccdd855562364dd0babc95dec59d59
frizzonelli <fabrizio.rizzonelli@gmail.com>
2018-11-15 14:24:21 +0100

    small fix

--- RNCustomerly.js
+++ RNCustomerly.js
@@ -1,62 +1,61 @@
-
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { RNCustomerly } = NativeModules;
 
 export default RNCustomerly;
 
-  registerUser(email, userId, name, attributes, company, callback) {
-	  RNCustomerly.registerUser(email, userId, name, attributes, company, callback);
-  }
+// registerUser(email, userId, name, attributes, company, callback) {
+//   RNCustomerly.registerUser(email, userId, name, attributes, company, callback);
+// }
 
-  logoutUser() {
-	  RNCustomerly.logoutUser((_null_) => { });
-  }
+// logoutUser() {
+//   RNCustomerly.logoutUser((_null_) => { });
+// }
 
-  openSupport() {
-	  RNCustomerly.openSupport((_null_) => { });
-  }
+// openSupport() {
+//   RNCustomerly.openSupport((_null_) => { });
+// }
 
-  isSdkAvailable(callback) {
-	  RNCustomerly.isSdkAvailable(callback);
-  }
+// isSdkAvailable(callback) {
+//   RNCustomerly.isSdkAvailable(callback);
+// }
 
-  setAttributes(attributes, callback) {
-	  RNCustomerly.setAttributes(attributes, callback);
-  }
+// setAttributes(attributes, callback) {
+//   RNCustomerly.setAttributes(attributes, callback);
+// }
 
-  setCompany(company, callback) {
-	  RNCustomerly.setCompany(attributes, callback);
-  }
+// setCompany(company, callback) {
+//   RNCustomerly.setCompany(attributes, callback);
+// }
 
-  setSupportEnabled(enabled, callback) {
-	  RNCustomerly.setSupportEnabled(enabled, callback);
-  }
+// setSupportEnabled(enabled, callback) {
+//   RNCustomerly.setSupportEnabled(enabled, callback);
+// }
 
-  isSupportEnabled(callback) {
-	  RNCustomerly.isSupportEnabled(callback);
-  }
+// isSupportEnabled(callback) {
+//   RNCustomerly.isSupportEnabled(callback);
+// }
 
-  setSurveyEnabled(enabled, callback) {
-	  RNCustomerly.setSurveyEnabled(enabled, callback);
-  }
+// setSurveyEnabled(enabled, callback) {
+//   RNCustomerly.setSurveyEnabled(enabled, callback);
+// }
 
-  isSurveyEnabled(callback) {
-	  RNCustomerly.isSurveyEnabled(callback);
-  }
+// isSurveyEnabled(callback) {
+//   RNCustomerly.isSurveyEnabled(callback);
+// }
 
-  trackEvent(eventName, callback) {
-	  RNCustomerly.trackEvent(eventName, callback);
-  }
+// trackEvent(eventName, callback) {
+//   RNCustomerly.trackEvent(eventName, callback);
+// }
 
-  update(callback) {
-	  RNCustomerly.update(callback);
-  }
+// update(callback) {
+//   RNCustomerly.update(callback);
+// }
 
-  setVerboseLogging(enabled, callback) {
-	  RNCustomerly.setVerboseLogging(enabled, callback);
-  }
+// setVerboseLogging(enabled, callback) {
+//   RNCustomerly.setVerboseLogging(enabled, callback);
+// }
 
-  setAttachmentsAvailable(enabled, callback) {
-	  RNCustomerly.setAttachmentsAvailable(enabled, callback);
-  }
+// setAttachmentsAvailable(enabled, callback) {
+//   RNCustomerly.setAttachmentsAvailable(enabled, callback);
+// }
--- ios/RNCustomerly.m
+++ ios/RNCustomerly.m
@@ -8,13 +8,16 @@
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(configure: (NSString*)appId widgetColor: (UIColor*)color){
+RCT_EXPORT_METHOD(configure: (NSString*)appId widgetColor: (UIColor*)color)
+{
     [[Customerly sharedInstance] configureWithAppId:appId widgetColor:color];
 }
 
-RCT_EXPORT_METHOD(openSupport: (UIViewController *)vc){
+RCT_EXPORT_METHOD(openSupport: (UIViewController *)vc)
+{
     [[Customerly sharedInstance] openSupportFrom:vc];
 }
 
