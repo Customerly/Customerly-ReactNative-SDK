456d62229b24b02f533f685fd3ace3643f5ef543
frizzonelli <fabrizio.rizzonelli@gmail.com>
2018-11-19 14:58:26 +0100

    added working callback for iOS

--- ios/RNCustomerly.m
+++ ios/RNCustomerly.m
@@ -1,12 +1,11 @@
 #import "RNCustomerly.h"
 #import "UIWindow+Customerly.h"
+#import <React/RCTBridgeModule.h>
 //#import <CustomerlySDK/CustomerlySDK-umbrella.h>
 
 @implementation RNCustomerly
 
 @synthesize bridge = _bridge;
-
-typedef void (^SuccessTestCallback)(void);
 
 - (dispatch_queue_t)methodQueue
 {
@@ -15,9 +14,16 @@ typedef void (^SuccessTestCallback)(void);
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(registerUser: (NSString*)email userId: (NSString*)userId name: (NSString*)name successCallback: (SuccessTestCallback)successCallback)
+RCT_EXPORT_METHOD(registerUser: (NSString*)email userId: (NSString*)userId name: (NSString*)name callback: (RCTResponseSenderBlock)callback)
 {
-    //    [[Customerly sharedInstance] registerUserWithEmail:email user_id:userId name:name];
+    [[Customerly sharedInstance] registerUserWithEmail:email user_id:userId name:name attributes:NULL company:NULL
+                                               success:^{
+                                                   NSArray *someData = @[@"Success"];
+                                                   callback(@[[NSNull null], someData]);
+                                               } failure:^{
+                                                   NSArray *someData = @[@"Failure"];
+                                                   callback(@[[NSNull null], someData]);
+                                               }];
 }
 
 RCT_EXPORT_METHOD(openSupport)
